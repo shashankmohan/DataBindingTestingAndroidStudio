3df74eab1fd1b7b875c2cc9dedd487b8e5c915f0
developer_shashank <developer.shank@gmail.com>
2018-05-31 22:20:02 +0530

    Initial commit

---
+++ app/src/main/java/self/android/com/databindingsample/MainActivity.java
@@ -0,0 +1,27 @@
+package self.android.com.databindingsample;
+
+import android.databinding.DataBindingComponent;
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import self.android.com.databindingsample.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private User user;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       /* ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        user = new User();
+
+        user.setName("Shashank Mohan");
+        user.setEmail("shashankunnithan03@gmail.com");
+        activityMainBinding.setUser(user);*/
+
+
+    }
+}
