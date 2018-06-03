21dc5d5c67f1aeac00d7f1698b9cc5ced22b9bb1
developer_shashank <developer.shank@gmail.com>
2018-06-03 15:31:58 +0530

    Data Binding with include in xml file

--- app/src/main/java/self/android/com/databindingsample/MainActivity.java
+++ app/src/main/java/self/android/com/databindingsample/MainActivity.java
@@ -1,9 +1,8 @@
 package self.android.com.databindingsample;
 
-import android.databinding.DataBindingComponent;
 import android.databinding.DataBindingUtil;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import self.android.com.databindingsample.databinding.ActivityMainBinding;
 
@@ -16,13 +15,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       /* ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
         user = new User();
-
         user.setName("Shashank Mohan");
-        user.setEmail("shashankunnithan03@gmail.com");
-        activityMainBinding.setUser(user);*/
+        user.setEmail("shashank@gmail.com");
 
-//This is my test code for databinding
+        binding.setUser(user);
     }
 }
