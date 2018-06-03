ad517d29b44291a6aabefe97e7561c27c233b48c
developer_shashank <developer.shank@gmail.com>
2018-06-03 15:53:01 +0530

    Data Binding with include in xml file

--- app/src/main/java/self/android/com/databindingsample/MainActivity.java
+++ app/src/main/java/self/android/com/databindingsample/MainActivity.java
@@ -1,14 +1,18 @@
 package self.android.com.databindingsample;
 
+import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 import self.android.com.databindingsample.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private User user;
+    MyClickHandlers handlers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +26,26 @@ public class MainActivity extends AppCompatActivity {
         user.setEmail("shashank@gmail.com");
 
         binding.setUser(user);
+
+        handlers = new MyClickHandlers(this);
+        binding.content.setHandlers(handlers);
+    }
+
+
+    public class MyClickHandlers {
+
+        Context context;
+
+        public MyClickHandlers(Context context) {
+            this.context = context;
+        }
+
+        public void onFabClicked(View view) {
+            Toast.makeText(context, "Fab Clicked", Toast.LENGTH_SHORT).show();
+        }
+
+        public void onButtonClickedWithParams(View view, User user) {
+            Toast.makeText(context, "Clicked " + user.getName(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
